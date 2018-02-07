e2fcdd900a0536d3122a09053e7cdc5edbaafe84
leepuppychow <leepuppychow@gmail.com>
2018-02-07 09:45:43 -0700

    Beginning get request to API endpoint; limit by zipcode and radius of 6 miles, fuel type ELEC and LPG

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,9 @@
 class SearchController < ApplicationController
 
   def index
+    connection = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1.json?zip=#{zipcode}&radius=6&fuel_type=LPG,ELEC&limit=10&api_key=#{ENV["NREL_API_KEY"]}&format=JSON")
+    response = JSON.parse(connection.body, symbolize_names: true)
+
     binding.pry
   end
 
